a91a6beca167c1e7b37dc5eeda0bb1ecaa8f7c0a
Dan Reynolds <me@danreynolds.ca>
2017-11-29 12:37:58 -0800

    hookup action router

---
+++ lib/action_router.rb
@@ -0,0 +1,18 @@
+class ActionRouter
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    input = env['rack.input']
+    body = input.read
+
+    unless body.blank?
+      relative_path = JSON.parse(body).with_indifferent_access[:result][:action]
+      env['PATH_INFO'] += relative_path
+    end
+
+    input.rewind
+    @app.call(env)
+  end
+end
